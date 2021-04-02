8c5730e34c0b6b2561879810633305f8b44655cf
rigaard-claus <80572832+rigaard-claus@users.noreply.github.com>
2021-04-02 17:55:13 +0300

    Add radiant chart

--- scr/helper.js
+++ scr/helper.js
@@ -1,3 +1,21 @@
 function InitHelper() {
-    
+    DrawMainChart();
+}
+
+function DrawMainChart() {
+    var json = '{"C#":9,"ASP":8,"PHP":8,"JS":10,"AS":6,"SQL":8}';
+    var area_size = 500;
+    rigaard_radiant_chart.init(json,area_size);
+    rigaard_radiant_chart.author(area_size);
+}
+
+function GetScript(url) {
+    $.ajax({
+        url: url,
+        dataType: 'script',
+        success: function() {
+            console.log(url + ' - load success');
+        },
+        async: false
+    });
 }
